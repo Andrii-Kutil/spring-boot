3520d29472937759a1e46a260c4639338faaf467
Superman696 <61060058+Superman696@users.noreply.github.com>
2020-07-10 19:37:38 +0300

    added FileReaderServiceImplTest

--- src/test/java/springboot/demo/service/impl/FileReaderServiceImplTest.java
+++ src/test/java/springboot/demo/service/impl/FileReaderServiceImplTest.java
@@ -1,18 +1,19 @@
 package springboot.demo.service.impl;
 
+
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import springboot.demo.model.dto.ReviewDto;
-import java.io.IOException;
+
 import java.util.List;
 
 class FileReaderServiceImplTest {
-    FileReaderServiceImpl fileReaderService = new FileReaderServiceImpl();
-    List<ReviewDto> dataFromCsv;
+    private static final String FILE_NAME = "test-reviews.csv";
+    private final FileReaderServiceImpl fileReaderService = new FileReaderServiceImpl();
 
     @Test
-    public void getDataFromCsvOk() throws IOException {
-        dataFromCsv = fileReaderService.getDataFromCsv("src/main/resources/test-reviews.csv");
+    public void getDataFromCsvOk() {
+        List<ReviewDto> dataFromCsv = fileReaderService.getDataFromCsv(FILE_NAME);
         Assert.assertEquals(3, dataFromCsv.size());
     }
 }
