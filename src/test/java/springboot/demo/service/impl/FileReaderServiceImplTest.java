91f31674e0204a50b013667ebefbf38b792752dd
Superman696 <61060058+Superman696@users.noreply.github.com>
2020-07-11 15:49:38 +0300

    added test for CsvReader

--- src/test/java/springboot/demo/service/impl/FileReaderServiceImplTest.java
+++ src/test/java/springboot/demo/service/impl/FileReaderServiceImplTest.java
@@ -3,17 +3,19 @@ package springboot.demo.service.impl;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-import springboot.demo.model.dto.ReviewDto;
+import springboot.demo.model.Review;
+import springboot.demo.service.CsvReader;
 
 import java.util.List;
 
 class FileReaderServiceImplTest {
     private static final String FILE_NAME = "test-reviews.csv";
-    private final FileReaderServiceImpl fileReaderService = new FileReaderServiceImpl();
+    private final CsvReader csvReader = new CsvReaderImpl();
 
     @Test
     public void getDataFromCsvOk() {
-        List<ReviewDto> dataFromCsv = fileReaderService.getDataFromCsv(FILE_NAME);
-        Assert.assertEquals(3, dataFromCsv.size());
+        List<Review> dataFromCsv = csvReader.getDataFromCsv(FILE_NAME);
+        dataFromCsv.forEach(System.out::println);
+        Assert.assertEquals(4, dataFromCsv.size());
     }
 }
