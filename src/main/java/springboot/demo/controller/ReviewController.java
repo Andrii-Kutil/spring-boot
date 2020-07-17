3b243603ea87becdff1db33af2b660ce229e0713
Superman696 <61060058+Superman696@users.noreply.github.com>
2020-07-17 10:44:06 +0300

    used pagination

--- src/main/java/springboot/demo/controller/ReviewController.java
+++ src/main/java/springboot/demo/controller/ReviewController.java
@@ -1,9 +1,14 @@
 package springboot.demo.controller;
 
-import java.util.Map;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import springboot.demo.model.dto.WordDto;
 import springboot.demo.service.impl.SortWords;
 
 @RestController
@@ -13,7 +18,15 @@ public class ReviewController {
     private SortWords sortWords;
 
     @GetMapping("/words/most-popular")
-    private Map<String, Long> findMostPopularWords() {
-        return sortWords.findMostPopularWords();
+    private List<WordDto> findMostPopularWords(
+            @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
+            @RequestParam(value = "limit", required = false, defaultValue = "1000") Integer limit) {
+
+        List<WordDto> mostPopularWords = sortWords.findMostPopularWords();
+        Pageable pageable = PageRequest.of(page, limit);
+        int start = (int) pageable.getOffset();
+        int end = Math.min((start + pageable.getPageSize()), mostPopularWords.size());
+        return new PageImpl<>(mostPopularWords.subList(start, end),
+                pageable, mostPopularWords.size()).getContent();
     }
 }
