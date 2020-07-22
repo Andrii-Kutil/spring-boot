cf1ac0e76a1cb2fd6cd722e0bbc79e80b2c5e42e
Superman696 <61060058+Superman696@users.noreply.github.com>
2020-07-22 16:23:24 +0300

    used dtos in mapper instead entities

--- src/main/java/springboot/demo/model/mapper/ReviewMapper.java
+++ src/main/java/springboot/demo/model/mapper/ReviewMapper.java
@@ -1,23 +1,36 @@
 package springboot.demo.model.mapper;
 
+import java.time.LocalDateTime;
 import org.springframework.stereotype.Component;
 import springboot.demo.model.Product;
 import springboot.demo.model.Review;
 import springboot.demo.model.User;
-import springboot.demo.model.dto.ReviewDto;
+import springboot.demo.model.dto.CsvReviewDto;
+import springboot.demo.model.dto.ReviewCreateDto;
 
 @Component
 public class ReviewMapper {
-    public Review getFullReview(ReviewDto reviewDto, User user, Product product) {
+    public Review getFullReview(CsvReviewDto csvReviewDto, User user, Product product) {
         return Review.builder()
-                .helpfulnessDenominator(reviewDto.getHelpfulnessDenominator())
-                .helpfulnessNumerator(reviewDto.getHelpfulnessNumerator())
-                .score(reviewDto.getScore())
-                .summary(reviewDto.getSummary())
-                .text(reviewDto.getText())
-                .time(reviewDto.getTime())
+                .helpfulnessDenominator(csvReviewDto.getHelpfulnessDenominator())
+                .helpfulnessNumerator(csvReviewDto.getHelpfulnessNumerator())
+                .score(csvReviewDto.getScore())
+                .summary(csvReviewDto.getSummary())
+                .text(csvReviewDto.getText())
+                .time(csvReviewDto.getTime())
                 .user(user)
                 .product(product)
+                .build();
+    }
+
+    public Review getReviewFromReviewCreateDto(ReviewCreateDto reviewtDto) {
+        return Review.builder()
+                .helpfulnessNumerator(reviewtDto.getHelpfulnessNumerator())
+                .helpfulnessDenominator(reviewtDto.getHelpfulnessDenominator())
+                .score(reviewtDto.getScore())
+                .summary(reviewtDto.getSummary())
+                .text(reviewtDto.getText())
+                .time(LocalDateTime.now())
                 .build();
     }
 }
