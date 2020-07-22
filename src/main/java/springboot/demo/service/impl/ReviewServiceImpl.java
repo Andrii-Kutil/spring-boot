2f3a6ffe34aaedda7f03d3dce50795e86d396f00
Superman696 <61060058+Superman696@users.noreply.github.com>
2020-07-22 16:21:44 +0300

    added save, update and delete methods

--- src/main/java/springboot/demo/service/impl/ReviewServiceImpl.java
+++ src/main/java/springboot/demo/service/impl/ReviewServiceImpl.java
@@ -5,13 +5,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import springboot.demo.model.Review;
 import springboot.demo.model.User;
+import springboot.demo.model.dto.ReviewEditDto;
 import springboot.demo.repository.ReviewRepository;
 import springboot.demo.service.ReviewService;
+import springboot.demo.service.UserService;
 
 @Service
 public class ReviewServiceImpl implements ReviewService {
     @Autowired
+    private UserService userService;
+
+    @Autowired
     private ReviewRepository reviewRepository;
+
+    @Override
+    public Review save(Review review, User user) {
+        review.setUser(user);
+        return reviewRepository.save(review);
+    }
 
     @Override
     public Review save(Review review) {
@@ -31,5 +42,23 @@ public class ReviewServiceImpl implements ReviewService {
     @Override
     public List<String> findAllText() {
         return reviewRepository.findAllText();
+    }
+
+    @Override
+    public void setReviewInfoById(ReviewEditDto reviewEditDto) {
+        reviewRepository.setReviewInfoById(reviewEditDto.getHelpfulnessNumerator(),
+                reviewEditDto.getHelpfulnessDenominator(),
+                reviewEditDto.getScore(), reviewEditDto.getSummary(),
+                reviewEditDto.getText(), reviewEditDto.getId());
+    }
+
+    @Override
+    public void delete(Long id) {
+        reviewRepository.deleteById(id);
+    }
+
+    public Review findById(Long id) {
+        return reviewRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Review not found for this id:" + id));
     }
 }
