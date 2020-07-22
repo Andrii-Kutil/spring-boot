6dc97467d4e2fd229f7ec00648ef049d78545ef3
Superman696 <61060058+Superman696@users.noreply.github.com>
2020-07-22 16:20:06 +0300

    added custom id generator for product and user

--- src/main/java/springboot/demo/model/Product.java
+++ src/main/java/springboot/demo/model/Product.java
@@ -1,11 +1,16 @@
 package springboot.demo.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+import springboot.demo.util.UseExistingOrGenerateSequenceIdGenerator;
 
 @Data
 @Entity
@@ -14,5 +19,15 @@ import lombok.NoArgsConstructor;
 @Table(name = "product")
 public class Product {
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ExistingOrGenerated")
+    @GenericGenerator(name = "ExistingOrGenerated", strategy =
+            "springboot.demo.util.UseExistingOrGenerateSequenceIdGenerator",
+            parameters = {
+                    @Parameter(name =
+                            UseExistingOrGenerateSequenceIdGenerator.VALUE_PREFIX_PARAMETER,
+                            value = "P_"),
+                    @Parameter(name =
+                            UseExistingOrGenerateSequenceIdGenerator.NUMBER_FORMAT_PARAMETER,
+                            value = "%010d")})
     private String id;
 }
