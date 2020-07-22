716f9581194bb28f3cbbcf533be985368841456d
Superman696 <61060058+Superman696@users.noreply.github.com>
2020-07-22 16:16:58 +0300

    inject admin and user

--- src/main/java/springboot/demo/service/InjectDataService.java
+++ src/main/java/springboot/demo/service/InjectDataService.java
@@ -3,7 +3,6 @@ package springboot.demo.service;
 import java.util.List;
 import springboot.demo.model.Product;
 import springboot.demo.model.Review;
-import springboot.demo.model.Role;
 import springboot.demo.model.User;
 
 public interface InjectDataService {
@@ -14,8 +13,4 @@ public interface InjectDataService {
     Iterable<Product> injectProductsToDataBase(List<Review> reviews);
 
     Iterable<User> injectUsersToDataBase(List<Review> reviews);
-
-    Iterable<Role> injectRolesToDataBase(List<Role> roles);
-
-    List<Role> createRoles();
 }
